0410ba50f62938a4d41bd851754973b458e0877a
cduring <164360883+cduring@users.noreply.github.com>
2026-01-17 18:52:30 -0500

    added CORS policy

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from models import Prompt, Game
 from generate import generate_trivia_game
 
-app = FastAPI()
+app = FastAPI(
+    title="TrivMe backend",
+    description="Backend for TrivMe",
+    version="0.0.1",
+    contact={"email": "chimdi.during@gmail.com"},
+)
+
+origins = [
+    "http://localhost:5173",
+    "https://localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the TrivMe Backend API"}
 
 @app.post("/generate", response_model=Game)
-def create_game(prompt: Prompt):
+async def create_game(prompt: Prompt):
     """
     Endpoint to receive a prompt and generate a trivia game.
     """
